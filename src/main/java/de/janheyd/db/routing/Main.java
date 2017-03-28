adf62d7942bbe000dbd5dbc19ac4177ad59e95e8
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2017-03-29 01:20:43 +0200

    added actual output to Main.main

--- src/main/java/de/janheyd/db/routing/Main.java
+++ src/main/java/de/janheyd/db/routing/Main.java
@@ -1,14 +1,20 @@
 package de.janheyd.db.routing;
 
 import de.janheyd.db.routing.bahnapi.BahnApi;
+import de.janheyd.db.routing.search.Route;
 import de.janheyd.db.routing.search.TravelSearch;
 
-import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class Main {
 
 	public static void main(String... args) {
-		new TravelSearch(new BahnApi()).findRoute("Oldenburg", "Karlsruhe", LocalDate.of(2017, 1, 1));
+		Optional<Route> result = new TravelSearch(new BahnApi()).findRoute("Oldenburg", "Karlsruhe", LocalDate.of(2017, 1, 1));
+		result.ifPresent(route -> {
+			route.getStops().forEach(stop -> {
+				System.out.println(stop.getArrival() + " -> " + stop.getLocation() + " -> " + stop.getDeparture());
+			});
+		});
 	}
 }
