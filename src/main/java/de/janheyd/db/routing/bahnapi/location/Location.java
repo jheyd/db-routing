a5f71ae73cc6d56d88feda3600d1502f3ac6032d
Jan Heyd <github@janheyd.de>
2017-04-04 14:48:21 +0200

    introduced lombok @EqualsAndHashcode, @ToString, @Getter to class Location

--- src/main/java/de/janheyd/db/routing/bahnapi/location/Location.java
+++ src/main/java/de/janheyd/db/routing/bahnapi/location/Location.java
@@ -1,7 +1,13 @@
 package de.janheyd.db.routing.bahnapi.location;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
+@EqualsAndHashCode
+@ToString
+@Getter
 public class Location {
 
 	private String id;
@@ -10,36 +16,5 @@ public class Location {
 	public Location(@JsonProperty("id") String id, @JsonProperty("name") String name) {
 		this.id = id;
 		this.name = name;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("Location[id=%s,name=%s]", id, name);
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-
-		Location location = (Location) o;
-
-		if (id != null ? !id.equals(location.id) : location.id != null) return false;
-		return name != null ? name.equals(location.name) : location.name == null;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = id != null ? id.hashCode() : 0;
-		result = 31 * result + (name != null ? name.hashCode() : 0);
-		return result;
 	}
 }
